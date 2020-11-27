0404c5fa677734856348e57d35b021fdbaf6ae96
артём Андреевич Гладков <67080812+Developer-genius@users.noreply.github.com>
2020-11-28 02:51:46 +0300

    new sites

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+ $(document).ready(function() { 
+    var button = $('#button-up');	
+    $(window).scroll (function () {
+      if ($(this).scrollTop () > 300) {
+        button.fadeIn();
+      } else {
+        button.fadeOut();
+      }
+  });	 
+  button.on('click', function(){
+  $('body, html').animate({
+  scrollTop: 0
+  }, 800);
+  return false;
+  });		 
+  });
+
+
+//   
+
+
+
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 1000);
+    return false;
+  });
